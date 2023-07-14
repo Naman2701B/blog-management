c198a3b02d5bf68197ee3506ee982c641c6cfe9e
unknown <namanbadlani27@gmail.com>
2023-07-14 17:00:36 +0530

    Implemented Commenting and all its features

--- src/pages/articleDetail/ArticleDetailPage.jsx
+++ src/pages/articleDetail/ArticleDetailPage.jsx
@@ -4,6 +4,8 @@ import BreadCrumbs from "../../components/BreadCrumbs";
 import { images } from "../../constants";
 import { Link } from "react-router-dom";
 import SuggestedPosts from "./container/SuggestedPosts";
+import CommentContainer from "../../components/comments/CommentsContainer";
+import SocialShareButtons from "../../components/SocialShareButtons";
 const breadCrumbsData = [
     { name: "Home", link: "/" },
     { name: "Blog", link: "/blog" },
@@ -79,13 +81,25 @@ const ArticleDetailPage = () => {
                             nibh tortor id aliquet lectus proin.
                         </p>
                     </div>
+                    <CommentContainer className="mt-10" logginedUserId="a" />
                 </article>
-                <SuggestedPosts
-                    header="Latest Article"
-                    posts={postsData}
-                    tags={tagsData}
-                    className="mt-8 lg:mt-0 lg:max-w-xs"
-                />
+                <div>
+                    <SuggestedPosts
+                        header="Latest Article"
+                        posts={postsData}
+                        tags={tagsData}
+                        className="mt-8 lg:mt-0 lg:max-w-xs"
+                    />
+                    <div className="mt-7">
+                        <h2 className="font-roboto font-medium text-dark-hard mb-4 md:text-xl">
+                            Share on
+                        </h2>
+                        <SocialShareButtons
+                            url={encodeURI("www.google.com")}
+                            title={encodeURIComponent("Google")}
+                        />
+                    </div>
+                </div>
             </section>
         </MainLayout>
     );
