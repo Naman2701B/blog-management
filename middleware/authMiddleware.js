313b4daaebacb6898a14b6f1c9c720113e72d2cf
unknown <namanbadlani27@gmail.com>
2023-07-16 01:42:58 +0530

    First Commit to Backend

---
+++ middleware/authMiddleware.js
@@ -0,0 +1,24 @@
+import pkg from "jsonwebtoken";
+import User from "../models/User.js";
+const { verify } = pkg;
+export const authGuard = async (req, res, next) => {
+    if (
+        req.headers.authorization &&
+        req.headers.authorization.startsWith("Bearer")
+    ) {
+        try {
+            const token = req.headers.authorization.split(" ")[1];
+            const { id } = verify(token, process.env.JWT_TOKEN);
+            req.user = await User.findById(id).select("-password");
+            next();
+        } catch (error) {
+            const err = new Error("Not authorized, Token failed");
+            err.statusCode = 401;
+            next(err);
+        }
+    } else {
+        let error = new Error("Not authorized, no token.");
+        error.statusCode = 401;
+        next(error);
+    }
+};
