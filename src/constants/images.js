cbc150d97719424e062fcfccac18ca14c42a7b3b
unknown <namanbadlani27@gmail.com>
2023-07-21 03:04:47 +0530

    Rendering Article Page Dynamically from DB

--- src/constants/images.js
+++ src/constants/images.js
@@ -2,13 +2,17 @@ import Logo from "../assets/Logo.svg";
 import HomePageDemo from "../assets/HomePageDemo.svg";
 import post1 from "../assets/Posts/post1.jpg";
 import postProfile from "../assets/Posts/post-profile.svg";
-import CTA from "../assets/CTA.png"; 
+import CTA from "../assets/CTA.png";
+const samplePostImage = "/images/sample.png";
+const userImage = "/images/user.png";
 const images = {
     Logo,
-    HomePageDemo, 
+    HomePageDemo,
     post1,
     postProfile,
-    CTA
+    CTA,
+    samplePostImage,
+    userImage,
 };
 
 export default images;
